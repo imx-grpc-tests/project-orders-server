b4f8399f7e1274ec684cce0e672bb0e14417b798
Maksym Hubach <mhuba@softserveinc.com>
2020-02-26 13:38:35 +0200

    Moved food database to JSON Added more logging

--- src/main/java/com/max/grpc/orders/server/Main.java
+++ src/main/java/com/max/grpc/orders/server/Main.java
@@ -8,6 +8,7 @@ import com.max.grpc.orders.server.services.ServicesHolder;
 public class Main {
     public static final int DEFAULT_SERVER_PORT = 16000;
     public static final String CONFIG_PATH = "src/main/resources/server.properties";
+    public static final String FOOD_DATABASE_PATH = "src/main/resources/food.json";
 
     public static void main(String[] args) {
         var config = new ServerConfig(CONFIG_PATH);
@@ -15,6 +16,7 @@ public class Main {
         int serverPort = config.getServerPortOrDefault(DEFAULT_SERVER_PORT);
 
         var food = new FoodManager();
+        food.loadFrom(FOOD_DATABASE_PATH);
         var services = new ServicesHolder(food);
         var controllers = new ControllersHolder(services);
         var cafeGrpcServer = new CafeServer(serverPort, controllers);
--- src/main/java/com/max/grpc/orders/server/food/FoodManager.java
+++ src/main/java/com/max/grpc/orders/server/food/FoodManager.java
@@ -1,23 +1,39 @@
+
 package com.max.grpc.orders.server.food;
 
-import com.max.grpc.orders.proto.FoodItem;
-import com.max.grpc.orders.proto.FoodType;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
+import com.max.grpc.orders.proto.FoodItem;
+
+import org.apache.log4j.Logger;
+
+import java.io.File;
+import java.io.IOException;
 import java.util.Arrays;
-import java.util.UUID;
 
 public class FoodManager {
-    private FoodItem[] foodItems = new FoodItem[] {
-        FoodItem.newBuilder()
-            .setId(UUID.randomUUID().toString())
-            .setTitle("Apple juice").setType(FoodType.DESSERT).setWeight(0.5f).setPrice(2).build(),
-        FoodItem.newBuilder()
-            .setId(UUID.randomUUID().toString())
-            .setTitle("Roast chicken").setType(FoodType.MAIN_COURSE).setWeight(1).setPrice(20).build(),
-        FoodItem.newBuilder()
-            .setId(UUID.randomUUID().toString())
-            .setTitle("Caesar salad").setType(FoodType.STARTERS).setWeight(0.3f).setPrice(3).build()
-    };
+    private final Logger logger = Logger.getLogger(FoodManager.class);
+    private FoodItem[] foodItems;
+    private ObjectMapper jsonMapper;
+    private FoodDbMapperImpl foodMapper;
+
+    public FoodManager() {
+        this.foodItems = new FoodItem[0];
+        this.jsonMapper = new ObjectMapper();
+        this.foodMapper = new FoodDbMapperImpl();
+    }
+
+    public void loadFrom(String filepath) {
+        logger.info("load-db: Loading DB from " + filepath);
+        try {
+            DbFoodItem[] fromJson = jsonMapper.readValue(new File(filepath), DbFoodItem[].class);
+            this.foodItems = foodMapper.toProtoModel(fromJson);
+            logger.info("load-db: Database loading completed, " + foodItems.length + " items available");
+        }
+        catch (IOException ex) {
+            logger.error("load-db: failed to load food database", ex);
+        }
+    }
 
     public FoodItem[] getMenu() {
         return foodItems;
